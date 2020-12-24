6a0e3ba72e10c54429ef0f345b0b04969c26d468
Sam Wang <samofcorinth@gmx.ca>
2020-12-23 22:24:51 -0800

    Unua kontrolpunkto

---
+++ spec/spec_helper.cr
@@ -0,0 +1,2 @@
+require "spec"
+require "../src/libro-de-krestia"
---
+++ src/libro-de-krestia.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Libro::De::Krestia`
+module KrestiaLibro
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
